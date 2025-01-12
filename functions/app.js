e7260ca05635e9a786c8de2d29c1f37ff749a944
hussamshannan <67448518+hussamshannan@users.noreply.github.com>
2025-01-12 16:34:12 +0300

    change in build command

--- functions/app.js
+++ functions/app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const serverless = require("serverless-http");
+// const serverless = require("serverless-http");
 const express = require('express');
 const upload = require('./routers/upload');
 const fetch = require('./routers/fetch');
@@ -12,7 +12,7 @@ const app = express();
 const corsOptions = {
     origin: "*",
 };
-const router = express.Router();
+// const router = express.Router();
 app.use(bodyParser.json());
 app.use(cors(corsOptions));
 
@@ -22,8 +22,8 @@ app.use('/delete', deleteFile);
 app.use('/storage', storage);
 app.use('/submit', submit);
 
-app.use("/.netlify/functions/app", router);
+// app.use("/.netlify/functions/app", router);
 app.listen("5000", () => {
     console.log("server is running")
 })
-module.exports.handler = serverless(app);
+// module.exports.handler = serverless(app);
